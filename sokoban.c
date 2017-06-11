87c1b97e65628e1ea657c5a7741bc98442aaa59f
dbsxotjr100 <dbsxotjr100@gmail.com>
2017-06-11 20:15:02 +0900

    Create sokoban.c

--- sokoban.c
+++ sokoban.c
@@ -149,7 +149,12 @@ printf("(command)");
 void save_map(int stage,double timer){//맵을 배열에 저장하는 함수
   FILE *sk;
   sk=fopen("sokoban","w");//소코반 파일을 덮어쓰기 위해서 열음
-fprintf(sk,"\n%.2fsec\n",(double)(timer-start)/CLOCKS_PER_SEC);//저장되어있는 점수를 기록함  
+  for(i=0;i<M;i++){  
+	  fprintf(sk,"%c",name[i]);//이름을 소코반 파일에 저장시킴  
+	  if(name[i]=='\n')//enter값을 받으면 반복문을 탈출하고 이름저장을 멈춤 
+		  break;  
+  }
+	fprintf(sk,"\n%.2fsec\n",(double)(timer-start)/CLOCKS_PER_SEC);//저장되어있는 점수를 기록함  
 	fprintf(sk,"map%d",s+1);//몇 번째 맵인지를 파일에 저장시킴  
 	fprintf(sk,"\n");  
 	for(i=stage;i<stage+1;i++){  
